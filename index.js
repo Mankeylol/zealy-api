47cd6a254d83aed290f5b3ba0b2bdd23dd4b06ad
mankeylol <mankeyooooo@gmail.com>
2023-11-15 10:29:22 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-const express = require('express')
-const axios = require('axios')
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors'); // Import the cors middleware
 
-const app = express()
-const PORT = 4000
+const app = express();
+const PORT = 4000;
+
+app.use(cors()); // Enable CORS for all routes
 
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+  console.log(`API listening on PORT ${PORT} `);
+});
 
 app.get('/', async (req, res) => {
-    try {
-        const response = await axios.get('https://api.zealy.io/communities/idklmao/leaderboard', {
-          headers: {
-            'x-api-key': '129f6dAgfoJBzk-XX1NskvpCe-H',
-          },
-          params: {
-            page: 0,
-            limit: 50,
-          },
-        });
-        const data = response.data;
-        console.log(data)
-        res.json(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(error.response?.status || 500).json({ error: error.message || 'Internal Server Error' });
-      }
-})
+  try {
+    const response = await axios.get('https://api.zealy.io/communities/idklmao/leaderboard', {
+      headers: {
+        'x-api-key': '129f6dAgfoJBzk-XX1NskvpCe-H',
+      },
+      params: {
+        page: 0,
+        limit: 50,
+      },
+    });
+    const data = response.data;
+    console.log(data);
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(error.response?.status || 500).json({ error: error.message || 'Internal Server Error' });
+  }
+});
 
-
-module.exports = app
+module.exports = app;
