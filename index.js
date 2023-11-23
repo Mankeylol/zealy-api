ef72e20854156f5f37128cb7543940dcd1eef7da
Mankeylol <91680427+Mankeylol@users.noreply.github.com>
2023-11-23 18:03:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
 
 const app = express();
 const PORT = 4000;
 
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
 
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
+  console.log(`API listening on PORT ${PORT}`);
 });
 
 app.get('/', async (req, res) => {
@@ -22,9 +22,23 @@ app.get('/', async (req, res) => {
         limit: 50,
       },
     });
-    const data = response.data;
-    console.log(data);
-    res.json(data);
+
+    console.log('Raw API Response:', response.data);
+
+    let data;
+
+    // Check if the response contains a 'leaderboard' array
+    if (Array.isArray(response.data.leaderboard)) {
+      data = response.data.leaderboard;
+    } else {
+      throw new Error('Unexpected response format - missing leaderboard array');
+    }
+
+    // Filter out entries with specific names
+    const filteredData = data.filter(entry => entry.name !== "Lzy Lad" && entry.name !== "kunal.zed" && entry.name !== "Dexterart.eth" && entry.name !== "Helllll.yeahhhh" && entry.name !== "Reet_s" && entry.name !== "creodevi.eth" && entry.name !== "ripfarhan" && entry.name !== "Wufm" && entry.name !== "Zordieee" && entry.name !== "svrnjxn" && entry.name !== "VALIPOKKANN" && entry.name !== "bhand.eth" && entry.name !== "Samuraizan" && entry.name !== "Yoddha.eth" );
+
+    console.log('Filtered Data:', filteredData);
+    res.json(filteredData);
   } catch (error) {
     console.error('Error fetching data:', error);
     res.status(error.response?.status || 500).json({ error: error.message || 'Internal Server Error' });
